cb6263fc80c32af36bf3110c7f41a046ba89237a
Slava S <slava.serbov@gmail.com>
2019-09-06 15:58:42 +0300

    Add init App state and Header component

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,11 +1,32 @@
-import React from 'react'
+import React, { Component } from 'react'
+import Header from './Header'
 
-const App = () => {
-    return (
-        <div className="text-center">
-            <h1>Hello World!</h1>
-        </div>
-    )
+class App extends Component {
+    state = {
+        poducts: [
+            {
+                name: "Potato",
+                count: 2
+            },
+            {
+                name: "Pasts",
+                count: 4
+            },
+            {
+                name: "Bananas",
+                count: 1
+            }
+        ]
+    }
+
+    render() {
+        return (
+            <div className="container border text-center mt-5">
+                <Header />
+                
+            </div>
+        )
+    }
 }
 
 export default App
---
+++ src/Components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+    return(
+        <div className="mt-3">
+            <h1>Shopping List</h1>
+        </div>
+    )
+}
+
+export default Header
