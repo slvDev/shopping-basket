d335a243b7cebef1e3f75853d736226afa1b8c91
Slava S <slava.serbov@gmail.com>
2019-09-06 15:19:35 +0300

    link CDN bootstrap and test it

--- src/Components/App.js
+++ src/Components/App.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 const App = () => {
     return (
-        <div>
-            <p>Hello World!</p>
+        <div className="text-center">
+            <h1>Hello World!</h1>
         </div>
     )
 }
