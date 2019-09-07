712c35704169e13a37e65405a5f42bc83d19917a
Slava S <slava.serbov@gmail.com>
2019-09-07 17:19:17 +0300

    add multi input to AddProductFrom

--- src/Components/AddProductForm.js
+++ src/Components/AddProductForm.js
@@ -3,9 +3,11 @@ import React from 'react'
 const AddProductForm = () => {
     return (
         <div className="input-group mb-3">
-            <input type="text" class="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="button-addon2" />
+            <input type="text" class="form-control" placeholder="Add Product" aria-describedby="button-addon2" />
+            <input type="text" class="form-control" placeholder="Add Count" aria-describedby="button-addon2" />
+            <input type="text" class="form-control" placeholder="Add Price" aria-describedby="button-addon2" />
             <div className="input-group-append">
-                <button className="btn btn-outline-secondary" type="button" id="button-addon2">Button</button>
+                <button className="btn btn-outline-secondary" type="submit" id="button-addon2">Add to List</button>
             </div>
         </div>
     )
