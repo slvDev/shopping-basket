1fc224bfed2d6bbb60bb0979605d5c33fc85976c
Slava S <slava.serbov@gmail.com>
2019-09-10 13:40:47 +0300

    Add BlankProduct and ProductList logic

---
+++ src/Components/BlankProduct.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const BlankProduct = () => {
+    return (
+        <div className="row my-4 h4 text-center">
+            <div className="col">
+                <h4>Add Product, count and price per item.</h4>
+            </div>
+            
+        </div>
+    )
+}
+
+export default BlankProduct
--- src/Components/ProductList.js
+++ src/Components/ProductList.js
@@ -1,19 +1,23 @@
 import React from 'react'
 import Product from './Product'
+import BlankProduct from './BlankProduct'
 
 const ProductList = (props) => {
+    const isFull = props.products.length
     return(
         <React.Fragment>
-            {props.products.map( (product, index) => 
-                <Product 
-                    key={product.id.toString()}
-                    name={product.name}
-                    count={product.count}
-                    price={product.price}
-                    id={product.id}
-                    deleteProduct={props.deleteProduct}
-                />
-            )}
+            {isFull 
+                ? props.products.map( product => 
+                    <Product 
+                        key={product.id.toString()}
+                        name={product.name}
+                        count={product.count}
+                        price={product.price}
+                        id={product.id}
+                        deleteProduct={props.deleteProduct}
+                    />)
+                : <BlankProduct />
+            }            
         </React.Fragment>
     )
 }
