cbab949faa99a0ce2ff9e3d81c00b1659606ad41
Slava S <slava.serbov@gmail.com>
2019-09-10 15:10:17 +0300

    Updates

---
+++ precache-manifest.daecb2527d4b829be637e2b6fad34822.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28ef460033ae0e7b21c977be46fa1ae1",
+    "url": "/shopping-basket/index.html"
+  },
+  {
+    "revision": "1211bac710ecce83d412",
+    "url": "/shopping-basket/static/css/main.4dc1f49a.chunk.css"
+  },
+  {
+    "revision": "1bb15bd4f8474f15782e",
+    "url": "/shopping-basket/static/js/2.18600160.chunk.js"
+  },
+  {
+    "revision": "1211bac710ecce83d412",
+    "url": "/shopping-basket/static/js/main.7f43fece.chunk.js"
+  },
+  {
+    "revision": "7d0102798d5a6d8858f8",
+    "url": "/shopping-basket/static/js/runtime~main.c34fde13.js"
+  }
+]);
